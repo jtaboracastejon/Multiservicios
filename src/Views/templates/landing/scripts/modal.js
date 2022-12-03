dc9b269e6cdf72fca37073b5a2c52999fbb3d510
jtaboracastejon <javiertaboracastejon@gmail.com>
2022-12-03 05:03:31 -0600

    Fixed:Spell, Added: Sweetalert & maxchars on vcode,

--- src/Views/templates/landing/scripts/modal.js
+++ src/Views/templates/landing/scripts/modal.js
@@ -1,16 +1,16 @@
-function modalHandler1(val = false,  toModal, fromModal = null) {
+function modalHandler1(val = false, toModal, fromModal = null) {
     let toModalTg = document.getElementById(toModal);
     let fromModalTg = document.getElementById(fromModal);
 
-    if(fromModalTg != null){
+    if (fromModalTg != null) {
         fadeOut(fromModalTg);
-    }    
+    }
     if (val) {
         fadeIn(toModalTg);
     } else {
         fadeOut(toModalTg);
     }
-    
+
 }
 function fadeOut(el) {
     el.style.opacity = 1;
@@ -53,43 +53,43 @@ function validationForm() {
         pswdError.innerHTML = "El campo no puede estar vacio";
         return error = true;
     }
-    
+
     return false;
-    
+
 }
 
-function clickLogin(){
-        if(!validationForm()){
-            console.log("Sin Error");
-            $.ajax({
-                type: "POST",
-                url: "index.php?page=sec_login",
-                data:{
-                    txtEmail: txtEmail.value,
-                    txtPswd: txtPswd.value,
-                    btnLogin: true
-                },
-                success: function (response) {
-                    if(response == "success"){
-                        window.location.href = "index.php?page=landing_landing";
-                    }
-                    else if(response == "error"){
-                        pswdError.innerHTML = "Usuario o contraseña incorrectos";
-                    }
-                    else if(response == "sinVerificarCorreo"){
-                        alert("Debe verificar su correo electronico");
-                        window.location.href = "index.php?page=landing_landing";
-                    }
+function clickLogin() {
+    if (!validationForm()) {
+        console.log("Sin Error");
+        $.ajax({
+            type: "POST",
+            url: "index.php?page=sec_login",
+            data: {
+                txtEmail: txtEmail.value,
+                txtPswd: txtPswd.value,
+                btnLogin: true
+            },
+            success: function (response) {
+                if (response == "success") {
+                    window.location.href = "index.php?page=landing_landing";
                 }
-                
-                
-            });
-        }
-        else{
-            console.log("Con Error");
-            return false;
-        }               
-    
+                else if (response == "error") {
+                    pswdError.innerHTML = "Usuario o contraseña incorrectos";
+                }
+                else if (response == "sinVerificarCorreo") {
+                    alert("Debe verificar su correo electronico");
+                    window.location.href = "index.php?page=landing_landing";
+                }
+            }
+
+
+        });
+    }
+    else {
+        console.log("Con Error");
+        return false;
+    }
+
 }
 
 
@@ -109,7 +109,7 @@ function validationFormUnete() {
         rbError.innerHTML = "Debe seleccionar una opcion";
         return true;
     }
-    else{
+    else {
         rbError.innerHTML = "";
         rbProveedor.checked == true ? cbUsuarios = "PRV" : cbUsuarios = "CLI";
         rbProveedor.checked == true ? userType.innerHTML = "Proveedor" : userType.innerHTML = "Cliente";
@@ -119,44 +119,44 @@ function validationFormUnete() {
         emailErrorUnete.innerHTML = "El campo no puede estar vacio";
         return true;
     }
-    else{
+    else {
         emailErrorUnete.innerHTML = "";
     }
-    
+
 
     return false;
 }
 
-function clickRegister1(){
-    if(!validationFormUnete()){
+function clickRegister1() {
+    if (!validationFormUnete()) {
         console.log("Sin Error");
         $.ajax({
             type: "POST",
             url: "index.php?page=sec_prueba",
-            data:{
+            data: {
                 txtEmail: txtEmailUnete.value,
                 btnRegister: true,
-                Cbusuarios : cbUsuarios,
+                Cbusuarios: cbUsuarios,
             },
             success: function (response) {
-                if(response == "success"){
+                if (response == "success") {
                     modalHandler1(true, 'modal2', 'modal1');
                 }
-                else if(response == "exist"){
+                else if (response == "exist") {
                     emailErrorUnete.innerHTML = "Correo ya existe";
                 }
-                else if(response == "error"){
+                else if (response == "error") {
                     emailErrorUnete.innerHTML = "Error al registrar";
                 }
             }
-            
-            
+
+
         });
     }
-    else{
+    else {
         console.log("Con Error");
         return false;
-    }               
+    }
 
 }
 
@@ -170,13 +170,13 @@ let cmbMuni = document.getElementById("cmbMuni");
 
 
 
-function validationRegister2(){
+function validationRegister2() {
     validatePassword(txtUserPass);
     if (txtUserName.value == "") {
         userNameError.innerHTML = "El campo no puede estar vacio";
         return true;
     }
-    else{
+    else {
         userNameError.innerHTML = "";
     }
 
@@ -184,7 +184,7 @@ function validationRegister2(){
         userPassError.innerHTML = "Debe llenar todos los campos";
         return true;
     }
-    else if(txtUserPass.value.length < 8 || txtConfirmPass.value.length < 8){
+    else if (txtUserPass.value.length < 8 || txtConfirmPass.value.length < 8) {
         userPassError.innerHTML = "La contraseña debe tener al menos 8 caracteres";
         return true;
     }
@@ -192,35 +192,35 @@ function validationRegister2(){
         userPassError.innerHTML = "Las contraseñas no coinciden";
         return true;
     }
-    else{
+    else {
         userPassError.innerHTML = "";
     }
 
     return false;
 }
 
-function clickRegister2(){
-    if(!validationRegister2()){
+function clickRegister2() {
+    if (!validationRegister2()) {
         console.log("Sin Error");
         $.ajax({
             type: "POST",
             url: "index.php?page=sec_register",
-            data:{
+            data: {
                 txtUser: txtUserName.value,
                 txtPswd: txtUserPass.value,
                 btnRegister2: true,
-                cbDepartamentos : cmbDepto.value,
-                cbMunicipios : cmbMuni.value,
+                cbDepartamentos: cmbDepto.value,
+                cbMunicipios: cmbMuni.value,
             },
             success: function (response) {
-                if(response == "success"){
+                if (response == "success") {
                     sendCorreo();
                     modalHandler1(true, 'modal3', 'modal2');
                 }
-                else if(response == "exist"){
+                else if (response == "exist") {
                     userNameError.innerHTML = "Credencial ya registrada en el sistema";
                 }
-                else if(response == "error"){
+                else if (response == "error") {
                     alert("Error al registrar");
                     window.location.href = "index.php?page=landing_landing";
                 }
@@ -237,37 +237,37 @@ function clickRegister2(){
 let txtCode = document.getElementById("txtCode");
 let codeError = document.getElementById("codeError");
 
-function validationRegister3(){
+function validationRegister3() {
     if (txtCode.value == "") {
         codeError.innerHTML = "El campo no puede estar vacio";
         return true;
     }
-    else{
+    else {
         codeError.innerHTML = "";
     }
 
     return false;
 }
 
-function clickRegister3(){
-    if(!validationRegister3()){
+function clickRegister3() {
+    if (!validationRegister3()) {
         console.log("Sin Error");
         $.ajax({
             type: "POST",
             url: "index.php?page=sec_verificacion",
-            data:{
+            data: {
                 txtVerificacion: txtCode.value,
                 step: "verificar",
             },
             success: function (response) {
-                if(response == "success"){
+                if (response == "success") {
                     alert("Cuenta verificada correctamente");
                     modalHandler1(true, 'modal4', 'modal3');
                 }
-                else if(response == "incorrecto"){
+                else if (response == "incorrecto") {
                     codeError.innerHTML = "El código de verificación es incorrecto";
                 }
-                else if(response == "error"){
+                else if (response == "error") {
                     codeError.innerHTML = "Ha ocurrido un error";
                 }
                 else {
@@ -280,66 +280,73 @@ function clickRegister3(){
 
 //Funciones
 
-function sendCorreo(){
+function sendCorreo() {
     $.ajax({
         type: "POST",
         url: "index.php?page=sec_verificacion",
-        data:{
+        data: {
             step: "enviarCorreo",
         },
         success: function (response) {
-            if(response == "success"){
+            if (response == "success") {
                 alert("¡Para verificar tu cuenta entra al link enviado a tu correo!");
             }
-            else{
-                alert("Error al enviar correo");
+            else {
+                
             }
         }
     });
 }
 
-function validatePassword(password){
-        if(password.value.match(/[a-z]/g) && password.value.match(/[A-Z]/g) && password.value.match(/[0-9]/g) && password.value.length >= 8){
-            userPassError.innerHTML = "";
+function validatePassword(password) {
+    if (password.value.match(/[a-z]/g) && password.value.match(/[A-Z]/g) && password.value.match(/[0-9]/g) && password.value.length >= 8) {
+        userPassError.innerHTML = "";
 
-            var strongRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
-            var mediumRegex = new RegExp("/^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\w\d\s:])([^\s]){8,32}$/");
+        var strongRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
+        var mediumRegex = new RegExp("/^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\w\d\s:])([^\s]){8,32}$/");
 
-            if(strongRegex.test(password)){
-                password.style.borderColor = "green";
-                return true;
-            }
-            else if(mediumRegex.test(password)){
-                password.style.borderColor = "orange";
-                return true;
-            }
-            else{
-                password.style.borderColor = "red";
-                return false;
-            }
+        if (strongRegex.test(password)) {
+            password.style.borderColor = "green";
+            return true;
         }
-        else{
-            userPassError.innerHTML = "La contraseña debe contener al menos 8 caracteres, una mayuscula, una minuscula y un número";
+        else if (mediumRegex.test(password)) {
+            password.style.borderColor = "orange";
+            return true;
+        }
+        else {
             password.style.borderColor = "red";
             return false;
         }
+    }
+    else {
+        userPassError.innerHTML = "La contraseña debe contener al menos 8 caracteres, una mayuscula, una minuscula y un número";
+        password.style.borderColor = "red";
+        return false;
+    }
     /* var re = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
     return re.test(password) && re.test(confirmPassword); */
 }
 
-function verificarEmail(email, errorMsj){
+function verificarEmail(email, errorMsj) {
     email.addEventListener("blur", function () {
-    let emails = email.value; 
-    let regex = /^([a-zA-Z0-9\.-]+)@([a-zA-Z0-9-]+).([a-z]{2,8})(.[a-z]{2,8})?$/;
-    if (regex.test(emails)) {
-        errorMsj.innerHTML = "";
-        error = false;
-        return true;
-    } else {
-        errorMsj.innerHTML = "Correo invalido";
-        errorMsj.style.color = "red";
-        error = true;
-        return false;
-    }
-})
+        let emails = email.value;
+        let regex = /^([a-zA-Z0-9\.-]+)@([a-zA-Z0-9-]+).([a-z]{2,8})(.[a-z]{2,8})?$/;
+        if (regex.test(emails)) {
+            errorMsj.innerHTML = "";
+            error = false;
+            return true;
+        } else {
+            errorMsj.innerHTML = "Correo invalido";
+            errorMsj.style.color = "red";
+            error = true;
+            return false;
+        }
+    })
 };
+/* swal.fire({
+    title: 'Error!',
+    text: 'Ha ocurrido un error con el servidor de correos intente mas tarde',
+    icon: 'error',
+    confirmButtonColor: '#1C3879',
+    confirmButtonText: 'Cerrar'
+}) */
